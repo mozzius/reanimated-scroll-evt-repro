f50e32d4237b96933ccf85c78688b941b50e533f
Samuel Newman <mozzius@protonmail.com>
2024-09-27 09:15:50 +0100

    Initial commit
    Generated by create-expo-app 3.0.0.

---
+++ app/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from 'expo-router';
+import 'react-native-reanimated';
+
+
+
+export default function RootLayout() {
+
+  return (
+      <Stack>
+        <Stack.Screen name="index"  />
+      </Stack>
+  );
+}
